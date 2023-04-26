94cf4fe149958e3709b22700c5349be55b5bf9c9
anna <9408commit@gmail.com>
2023-04-26 20:10:25 +0900

    이진트리 문제 2번 Maximum Depth of Binary Tree

---
+++ chpa04/6_mdbt.py
@@ -0,0 +1,20 @@
+# Maximum Depth of Binary Tree
+
+
+from collections import deque
+
+def mdbt(root):
+    level = 0
+    if root is None:
+        return 0
+    q = deque()
+    q.append((root, 1))
+    while q:
+        cur_node, cur_depth = q.popleft()
+        level = cur_depth
+        if cur_node.left:
+            q.append((cur_node.left, cur_depth + 1))
+        if cur_node.right:
+            q.append((cur_node.right, cur_depth + 1))
+
+    return level
